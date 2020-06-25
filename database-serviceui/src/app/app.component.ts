302e1dd84a5881eea39965c00cfa5b4f0edc9749
trod <st104680@stud.uni-stuttgart.de>
2020-06-25 08:36:36 +0200

    Added sending config to backend SLADIM-32

--- database-serviceui/src/app/app.component.ts
+++ database-serviceui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'database-serviceui';
+
+  constructor(private http: HttpClient) {}
+
+  backendUrl = 'http://localhost:3000';
+  // two way binding to ui inputs
+  responseSuccessChecked = false;
+  correctResponseChecked = false;
+  responseTime = 5000;
+  cpuUtilizationLoad = 60;
+
+  createMessage(): JSON {
+    const message = '{"responseSuccessChecked" : "'
+      + this.responseSuccessChecked + '", "correctResponseChecked" : "'
+      + this.correctResponseChecked + '", "responseTime" : "'
+      + this.responseTime + '", "cpuUtilizationLoad" : "'
+      + this.cpuUtilizationLoad + '" }';
+    return JSON.parse(message);
+  }
+
+  saveConfig() {
+    this.http.post(this.backendUrl, this.createMessage()).subscribe(
+      res => console.log(`Report sent to monitor at ${this.backendUrl}`),
+      err => console.log(`Monitor at ${this.backendUrl} not available`),
+    );
+  }
 }
--- database-serviceui/src/app/app.module.ts
+++ database-serviceui/src/app/app.module.ts
@@ -2,8 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 // angular material imports
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,9 +18,15 @@ import { MatToolbarModule } from "@angular/material/toolbar";
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
-
-    MatToolbarModule
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
