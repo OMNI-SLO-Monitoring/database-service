bf0d3399ba1ab8bbde73cfb3650fb9b85cdc85a0
Marcel Wolkober <st163937@stud.uni-stuttgart.de>
2020-06-29 16:09:30 +0200

    Added Swagger Doc, SLADIM-36
    doc at http://localhost:3000/api, json file at: http://localhost:3000/api-json

--- database-service/src/main.ts
+++ database-service/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  //swagger IO init
+  const options = new DocumentBuilder()
+    .setTitle('Database-Service Swagger Documentation')
+    .setDescription('This service has the following API')
+    .setVersion('1.0')
+    .addTag('database')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
