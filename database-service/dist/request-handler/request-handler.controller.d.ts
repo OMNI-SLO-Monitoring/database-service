45ca95682c02bbf0586de7546fd2ead197b4be50
Marcel Wolkober <st163937@stud.uni-stuttgart.de>
2020-07-23 11:43:52 +0200

    remove Dist folder SLADIM-100

--- database-service/dist/request-handler/request-handler.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { RequestHandlerService } from './request-handler.service';
-export declare class RequestHandlerController {
-    private requestHandler;
-    constructor(requestHandler: RequestHandlerService);
-    retrieveBalance(): Promise<any>;
-    retrieveCustomerName(): Promise<unknown>;
-    addCustomer(customer: any): any;
-}
