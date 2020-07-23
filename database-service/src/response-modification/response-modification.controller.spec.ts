eb5c379579bcfee72343aa05c7f339c9f1f3e666
Jiaxuan He <jiaxuan_he@yahoo.de>
2020-07-23 16:41:06 +0200

    fixed tests

--- database-service/src/response-modification/response-modification.controller.spec.ts
+++ database-service/src/response-modification/response-modification.controller.spec.ts
@@ -2,37 +2,42 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ResponseModificationController } from './response-modification.controller';
 import { AppService } from '../app.service';
 import { CpuUtilizationService } from 'cpu-utilization-observer';
-import { CpuUtilizationModule } from "cpu-utilization-observer";
+import { CpuUtilizationModule } from 'cpu-utilization-observer';
 import { RequestHandlerService } from '../request-handler/request-handler.service';
 
-
 describe('ResponseModification Controller', () => {
-
-jest.mock("../app.service")
-jest.mock("cpu-utilization-observer")
-jest.mock('../request-handler/request-handler.service')
+  jest.mock('../app.service');
+  jest.mock('cpu-utilization-observer');
+  jest.mock('../request-handler/request-handler.service');
 
   let responseModificationController: ResponseModificationController;
   let appService: AppService;
-  let requestHandlerService : RequestHandlerService;
+  let requestHandlerService: RequestHandlerService;
+  let cpuUtilizationService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports:[CpuUtilizationService, CpuUtilizationModule],
+      imports: [CpuUtilizationService, CpuUtilizationModule],
       controllers: [ResponseModificationController],
-      providers:   [AppService, RequestHandlerService]
+      providers: [AppService, RequestHandlerService, CpuUtilizationService],
     }).compile();
 
-    responseModificationController = module.get<ResponseModificationController>(ResponseModificationController);
+    responseModificationController = module.get<ResponseModificationController>(
+      ResponseModificationController,
+    );
     appService = module.get<AppService>(AppService);
-    requestHandlerService = module.get<RequestHandlerService>(RequestHandlerService);
+    requestHandlerService = module.get<RequestHandlerService>(
+      RequestHandlerService,
+    );
+    cpuUtilizationService = module.get<CpuUtilizationService>(
+      CpuUtilizationService,
+    );
   });
 
   it('should be defined', () => {
     expect(responseModificationController).toBeDefined();
   });
-  
+
   afterEach(() => {
     jest.resetAllMocks();
- });
-  
+  });
 });
--- database-serviceui/src/app/app.component.spec.ts
+++ database-serviceui/src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   }));
 
